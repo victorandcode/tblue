1290b1d4c99ccef429f0efa32f122bb52fb97334
Victor <azeta351@gmail.com>
2019-01-13 14:34:04 +0100

    feat: Add list creation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import { create } from "~/api/board";
+import { create as createBoard } from "~/api/board";
+import { create as createList } from "~/api/list";
 import logger from "~/common/logger";
 
 function main() {
-  create("Created from console yey")
-    .then(response => logger.success("It was created successufully"))
+  createBoard("My board with list")
+    .then(response => {
+      logger.success(
+        `Board was created successufully an has an id of ${response.data.id}`
+      );
+      /*createList("To Do", response.data.id)
+        .then(response => {
+          logger.success("List was created");
+        })
+        .catch(error => logger.fail(`Error is: ${error}`));*/
+    })
     .catch(error => logger.fail(`ERROR is: ${error}`));
 }
 
