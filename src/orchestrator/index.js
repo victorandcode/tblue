3645b1e9bed843029a4ba88f456f4f1e5ddd8b92
Victor <azeta351@gmail.com>
2019-01-19 19:28:23 +0100

    feat: Add user story cards

--- src/orchestrator/index.js
+++ src/orchestrator/index.js
@@ -1,12 +1,15 @@
 // @flow
 import { solicitQuestionnaire } from './questionnaire';
 import { askQuestions } from './questions';
+import { solicitUserStories } from './userStories';
 
 export const begin = async () => {
     const questionnaire = await solicitQuestionnaire();
     const questionGeneratedCards = await askQuestions(questionnaire);
+    const userStoryCards = await solicitUserStories();
     return [
         ...questionGeneratedCards,
-        questionnaire.cards,
+        ...questionnaire.cards,
+        ...userStoryCards,
     ];
 };
---
+++ src/orchestrator/tests/userStories.spec.js
@@ -0,0 +1,18 @@
+jest.mock('inquirer');
+
+import inquirer from 'inquirer';
+import { solicitUserStories, questionName } from '../userStories';
+
+describe('userStories', () => {
+    describe('when user adds 1 story', () => {
+        it('generates corresponding card', async () => {
+            inquirer.prompt.mockImplementation(() => Promise.resolve({
+                questionName: 'Search for products'
+            }));
+            const [card] = await solicitUserStories();
+            expect(card).toEqual({
+                "name": "Search for products"
+            });
+        });
+    });
+});
