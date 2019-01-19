cc4d994fbda0ae4d67c253346f0ece2b23be8043
Victor <azeta351@gmail.com>
2019-01-19 23:13:43 +0100

    feat: Provide option to open board before process ends

--- src/orchestrator/index.js
+++ src/orchestrator/index.js
@@ -1,4 +1,7 @@
 // @flow
+import opn from 'opn';
+import inquirer from 'inquirer';
+
 import logger from '~/common/logger';
 
 import type { Card } from '~/templates/types';
@@ -32,10 +35,25 @@ const registerData = async (boardName: string, cards: Array<Card>) => {
         logger.info(`Created ${i+1}/${cards.length} cards`)
     }
     logger.success('All cards created!');
+    return board;
+};
+
+export const solicitOpenBoardUrl = async (board) => {
+    const questionName = 'openBoardUrl';
+    const answer = await inquirer
+        .prompt([{
+            type: 'confirm',
+            name: questionName,
+            message: `Board url is ${board.url}, would you like to open it?`,
+        }])
+    if(answer[questionName]) {
+        opn(board.url);
+    }
 };
 
 export const begin = async () => {
     const boardName = await solicitBoardName();
     const cards = await getCards();
-    await registerData(boardName, cards);
+    const board = await registerData(boardName, cards);
+    await solicitOpenBoardUrl(board);
 };
