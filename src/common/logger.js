611c903036573b65b545484b4cd25721a3efde47
Victor <azeta351@gmail.com>
2019-01-20 17:31:49 +0100

    feat: Add figure support and improve logger instructions helper

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,13 +1,17 @@
 // @flow
 import colors from 'colors';
+import figures from 'figures';
 
-const fail = (str: string) => console.log(colors.red(str));
-const instructions = (str: string) => console.log(colors.underline(str));
-const success = (str: string) => console.log(colors.green(str));
-const warning = (str: string) => console.log(colors.yellow(str));
+const base = (str: string) => console.log(figures(str));
+
+const fail = (str: string) => base(colors.red(str));
+const instructions = (str: string) => base(colors.italic(str));
+const success = (str: string) => base(colors.green(str));
+const warning = (str: string) => base(colors.yellow(str));
 
 
 export default {
+    base,
     fail,
     instructions,
     success,
