11775773367e1d3b1bf63cd411f37f56bd68c6fe
Victor <azeta351@gmail.com>
2019-01-20 18:24:16 +0100

    feat: Add .env validation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // @flow
+import { validateEnvironment } from "~/common/env";
 import logger from "~/common/logger";
 import { begin } from "~/orchestrator";
 
 async function main() {
   try {
+    validateEnvironment(); 
     begin();
   } catch (error) {
-    logger.fail(`ERROR is: ${error}`);
+    logger.fail(`ERROR: ${error}`);
   }
 }
 
