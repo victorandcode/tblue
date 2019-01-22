20f13c149a200766ff10880492ec1bd805212aba
Victor <azeta351@gmail.com>
2019-01-22 23:23:46 +0100

    style: Add instructions for questionnaire selection

--- src/orchestrator/questionnaire.js
+++ src/orchestrator/questionnaire.js
@@ -1,5 +1,6 @@
 // @flow
 import inquirer from 'inquirer';
+import logger from '~/common/logger';
 import { loadQuestionnaires } from '~/templates/questionnaires';
 import type { Questionnaire } from '~/templates/types';
 
@@ -7,6 +8,8 @@ const questionName = 'selectedQuestionnaire';
 
 export const solicitQuestionnaire = async (): Questionnaire => {
     const questionnaires = loadQuestionnaires();
+    logger.instructions(
+        'Questionnaires contain pre-defined cards and also cards generated based on your answers');
     const answers = await inquirer
         .prompt([{
             type: 'list',
