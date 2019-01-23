1bcb379ebf4c27e13a2c439968e45e2883a0c7e9
Victor <azeta351@gmail.com>
2019-01-23 20:36:02 +0100

    style: Improve style for instructions

--- src/common/logger.js
+++ src/common/logger.js
@@ -5,7 +5,7 @@ import figures from 'figures';
 const base = (str: string) => console.log(figures(str));
 
 const fail = (str: string) => base(colors.red(str));
-const instructions = (str: string) => base(colors.italic(str));
+const instructions = (str: string) => base(colors.black(colors.bgWhite(str)));
 const success = (str: string) => base(colors.green(str));
 const warning = (str: string) => base(colors.yellow(str));
 
