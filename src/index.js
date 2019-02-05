ef1391e478d1d038fbf1fca19dc2612d09946c70
Victor <azeta351@gmail.com>
2019-02-05 21:13:50 +0100

    feat: Initial webpack config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // @flow
-import { validateEnvironment } from '~/common/env';
-import logger from '~/common/logger';
-import { printAppInfo } from './branding';
-import { begin } from '~/orchestrator';
+import { validateEnvironment } from "~/common/env";
+import logger from "~/common/logger";
+import { printAppInfo } from "./branding";
+import { begin } from "~/orchestrator";
 
 async function main() {
-    try {
-        printAppInfo();
-        await validateEnvironment();
-        await begin();
-    } catch (error) {
-        logger.fail(`${error.stack}`);
-    }
+  try {
+    printAppInfo();
+    await validateEnvironment();
+    await begin();
+  } catch (error) {
+    logger.fail(`${error.stack}`);
+  }
 }
 
 main();
