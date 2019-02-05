d02bedf0fba9d8377b78400bcaccae19397e5c8e
Victor <azeta351@gmail.com>
2019-02-05 22:37:20 +0100

    refactor: Rename alertAndOpenUrl

--- src/common/env.js
+++ src/common/env.js
@@ -16,7 +16,7 @@ const appKeyUrl = 'https://trello.com/app-key';
 
 const appTokenUrl = (apiKey: string) => `https://trello.com/1/authorize?key=${apiKey}&name=TmanScaffoldingForTrello&expiration=never&response_type=token&scope=read,write`;
 
-export const alertAndOpenUrl = async (missingEnvKey: string, url: string) => {
+export const stopAndDirectUserToUrl = async (missingEnvKey: string, url: string) => {
     logger.instructions(
         `You don\'t have the environment variable ${missingEnvKey} set. Press any key to open the URL where you can grab the value for it.`
     );
@@ -29,11 +29,11 @@ export const alertAndOpenUrl = async (missingEnvKey: string, url: string) => {
 export const validateEnvironment = async () => {
     const apiKey = getTrelloApiKey();
     if(!apiKey) {
-        await alertAndOpenUrl(envKeys.trelloApiKey, appKeyUrl);
+        await stopAndDirectUserToUrl(envKeys.trelloApiKey, appKeyUrl);
     }
 
     const appToken = getAppToken();
     if(!appToken) {
-        await alertAndOpenUrl(envKeys.appToken, appTokenUrl(apiKey));
+        await stopAndDirectUserToUrl(envKeys.appToken, appTokenUrl(apiKey));
     }
 };
