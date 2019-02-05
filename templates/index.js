eb482d9256fd40df41add58109235bf1cc98f119
Victor <azeta351@gmail.com>
2019-02-05 22:38:20 +0100

    refactor: Make module to expose templates as array

---
+++ templates/index.js
@@ -0,0 +1,9 @@
+import reactSideProjectTemplate from './react-side-project.json';
+import simpleApiTemplate from './simple-api.json';
+import wordpressBlogTemplate from './wordpress-blog.json';
+
+export default [
+    reactSideProjectTemplate,
+    simpleApiTemplate,
+    wordpressBlogTemplate,
+];
