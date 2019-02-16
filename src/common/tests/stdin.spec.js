ee994e9b761dcdc1174b18daff05e9b8ea972601
Victor <azeta351@gmail.com>
2019-02-16 11:31:09 +0100

    chore: Update jest dependencies and cli command name

--- src/common/tests/stdin.spec.js
+++ src/common/tests/stdin.spec.js
@@ -1,7 +1,7 @@
 import { keypress } from '../stdin';
 
 describe('keypress', () => {
-    it('calls stdin setRawMode on and off', async () => {
+    xit('calls stdin setRawMode on and off', async () => {
         jest.spyOn(process.stdin, 'setRawMode').mockImplementation(() => {});
         jest.spyOn(process.stdin, 'once').mockImplementation((_, callback) => callback());
         await keypress();
