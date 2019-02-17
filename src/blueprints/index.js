c59ee2110290681de8f90236ceb0c18f65c72489
Victor <azeta351@gmail.com>
2019-02-17 18:18:33 +0100

    feat: Add Learn a language blueprint

--- src/blueprints/index.js
+++ src/blueprints/index.js
@@ -1,9 +1,11 @@
 import reactSideProject from './react-side-project.json';
 import simpleApi from './simple-express-api.json';
 import wordpressBlog from './wordpress-blog.json';
+import learnALanguage from './learn-a-language.json'; //Inspired by https://trello.com/b/RlriGCBr/apprendre-une-langue
 
 export const builtInBlueprints = [
     reactSideProject,
     simpleApi,
     wordpressBlog,
+    learnALanguage,
 ];
